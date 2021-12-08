d08baffd7eb3dcc1ebe97ba8ae54563797b207cc
minhchien635 <minhchien0407.vk.1718@gmail.com>
2021-12-08 10:58:46 +0700

    add encryption

--- src/main/java/Utils/JsonToTimetable.java
+++ src/main/java/Utils/JsonToTimetable.java
@@ -7,16 +7,11 @@ import DTO.TimeTable;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class Api {
+public class JsonToTimetable {
     private static ArrayList<TimeTable> addTimeTables(JSONArray jsonTimeTables) {
         ArrayList<TimeTable> timeTables = new ArrayList<>();
 
@@ -90,6 +85,14 @@ public class Api {
 
             ClassSession classSession = new ClassSession();
 
+            JSONArray weeks = jsonObject.getJSONArray("week");
+            int length = weeks.length();
+            classSession.weeks = new boolean[length];
+
+            for (int i = 0; i < length; i++) {
+                classSession.weeks[i] = (boolean) weeks.get(i);
+            }
+
             classSession.start = jsonObject.getInt("start");
             classSession.day = jsonObject.getInt("day");
             classSession.length = jsonObject.getInt("length");
@@ -99,35 +102,16 @@ public class Api {
         return classSessions;
     }
 
-    public static ArrayList<TimeTable> getTimeTables(String params) throws IOException {
-        ArrayList<TimeTable> timeTables;
-        BufferedReader in = null;
-        String line = "";
-        String api = "https://timetable-heroku.herokuapp.com/api" + params;
+    public static ArrayList<TimeTable> covert(String data) throws IOException {
+        ArrayList<TimeTable> timeTables = new ArrayList<>();
 
-        try {
-            URLConnection urlConnection = new URL(api).openConnection();
-            InputStream is = urlConnection.getInputStream();
-            in = new BufferedReader(new InputStreamReader(is, "UTF-8"));
-            line = in.readLine();
+        if (!data.isEmpty()) {
+            JSONObject object = new JSONObject(data);
 
-            if (line != null) {
-                JSONObject object = new JSONObject(line);
+            JSONArray jsonTimeTables = object.getJSONArray("timeTables");
 
-                JSONArray jsonTimeTables = object.getJSONArray("timeTables");
-                timeTables = addTimeTables(jsonTimeTables);
-
-                System.out.println(api);
-                System.out.println(jsonTimeTables);
-
-                in.close();
-                return timeTables;
-            }
-        } catch (IOException e) {
-            in.close();
-            e.printStackTrace();
+            timeTables = addTimeTables(jsonTimeTables);
         }
-        in.close();
-        return null;
+        return timeTables;
     }
 }
